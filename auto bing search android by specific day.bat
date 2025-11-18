97e400b7198b2a72eb2b2181b36f241dc83e7fdd
ledoxmedox <101424523+ledoxmedox@users.noreply.github.com>
2025-11-18 05:43:55 -0800

    Add files via upload

---
+++ auto bing search android by specific day.bat
@@ -0,0 +1,132 @@
+@echo off
+
+for /f "tokens=2 delims==" %%a in ('wmic path win32_localtime get dayofweek /value') do set dow=%%a
+
+IF %dow% == 0 SET day=NEQVEPORROQVISQVAMES
+IF %dow% == 1 SET day=TQVIDOLOREMIPSVMQVIA
+IF %dow% == 2 SET day=DOLORSITAMETCONSECTA
+IF %dow% == 3 SET day=Anovelisalongnarrati
+IF %dow% == 4 SET day=easyandefficienteasy
+IF %dow% == 5 SET day=findreadingsentences
+IF %dow% == 6 SET day=RemovingTheSpacesOke
+
+echo Inserting %day% and then search!
+
+echo.
+title [1/20]
+echo [1/20]
+adb shell "input text '%day%'; sleep 0.6; input keyevent 66"
+timeout 10
+
+echo.
+title [2/20]
+echo [2/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [3/20]
+echo [3/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [4/20]
+echo [4/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [5/20]
+echo [5/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [6/20]
+echo [6/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [7/20]
+echo [7/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [8/20]
+echo [8/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [9/20]
+echo [9/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [10/20]
+echo [10/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [11/20]
+echo [11/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [12/20]
+echo [12/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [13/20]
+echo [13/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [14/20]
+echo [14/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [15/20]
+echo [15/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [16/20]
+echo [16/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [17/20]
+echo [17/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [18/20]
+echo [18/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [19/20]
+echo [19/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
+timeout 10
+
+echo.
+title [20/20]
+echo [20/20]
+adb shell "input tap 650 150; sleep 0.6; input keyevent 67; input keyevent 66"
---
+++ auto news new.bat
@@ -0,0 +1,49 @@
+adb shell "input tap 820 1800"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input tap 280 1800"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input tap 820 1150"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input tap 280 1150"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input tap 800 530"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input tap 280 530"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input swipe 500 500 500 1500 100"
+timeout 1
+
+adb shell "input tap 280 530"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input tap 800 530"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input tap 280 1150"
+timeout 5
+adb shell "input tap 746 2210"
+timeout 2
+
+adb shell "input tap 820 1150"
